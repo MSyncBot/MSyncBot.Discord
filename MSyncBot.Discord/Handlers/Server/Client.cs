bae811f2b43a4bb8a8704964968d98c060aabb1e
Neelov Eugene <neelov.ev@mail.ru>
2023-12-10 14:45:54 +0300

    Added ServerHandler.cs and Client.cs for work with server (syncing messages)

---
+++ MSyncBot.Discord/Handlers/Server/Client.cs
@@ -0,0 +1,24 @@
+﻿namespace MSyncBot.Discord.Handlers.Server
+{
+    public class Client
+    {
+        public string Name { get; }
+        public ClientType ClientType { get; set; }
+        public string? Message { get; set; }
+
+        public Client(string name, ClientType clientType, string? message = null)
+        {
+            Name = name;
+            ClientType = clientType;
+            Message = message;
+        }
+    }
+
+    public enum ClientType
+    {
+        Telegram,
+        Discord,
+        VK,
+        None
+    }
+}
