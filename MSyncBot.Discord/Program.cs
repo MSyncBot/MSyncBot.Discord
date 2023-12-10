d93b60353f706f46a366d6f9c09d2c712590106a
Neelov Eugene <neelov.ev@mail.ru>
2023-12-10 14:44:08 +0300

    Added main method

--- MSyncBot.Discord/Program.cs
+++ MSyncBot.Discord/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace MSyncBot.Discord;
+﻿using MLoggerService;
+
+namespace MSyncBot.Discord;
 
 class Program
 {
-    private static void Main()
+    private static async Task Main()
     {
-        
+        var bot = new Bot("####",
+            new MLogger(),
+            new MDatabase.MDatabase("####", "####", "####", "####"));
+        await bot.StartAsync();
+        await Task.Delay(-1);
     }
 }
