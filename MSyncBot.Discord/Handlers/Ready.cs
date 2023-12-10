c56901fc6825e3de4c29daef61a1ded2e2daaffb
Neelov Eugene <neelov.ev@mail.ru>
2023-12-10 14:48:00 +0300

    Added Ready.cs for get bot ready info

---
+++ MSyncBot.Discord/Handlers/Ready.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace MSyncBot.Discord.Handlers;
+
+public static class Ready
+{
+    public static Task ReadyHandler(DiscordClient sender, ReadyEventArgs e)
+    {
+        Bot.Logger.LogSuccess("Bot is ready to receive events.");
+        return Task.CompletedTask;
+    }
+}
