ae738fc08ad86bc486ffe6133f72ba70783623a8
Neelov Eugene <neelov.ev@mail.ru>
2023-12-10 14:46:25 +0300

    Added Error.cs for get bot errors (api errors)

---
+++ MSyncBot.Discord/Handlers/Error.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace MSyncBot.Discord.Handlers;
+
+public class Error
+{
+    public static Task ErrorHandler(DiscordClient sender, ClientErrorEventArgs e)
+    {
+        Bot.Logger.LogError(e.Exception.Message);
+        return Task.CompletedTask;
+    }   
+}
