cacc23eacb77ed7146bc088e474de445da6c0f09
Neelov Eugene <neelov.ev@mail.ru>
2023-12-17 19:17:09 +0300

    Rename class (Ready -> ReadyHandler) Rename method (ReadyHandler -> GetReady)

--- MSyncBot.Discord/Handlers/ReadyHandler.cs
+++ MSyncBot.Discord/Handlers/ReadyHandler.cs
@@ -3,9 +3,9 @@ using DSharpPlus.EventArgs;
 
 namespace MSyncBot.Discord.Handlers;
 
-public static class Ready
+public static class ReadyHandler
 {
-    public static Task ReadyHandler(DiscordClient sender, ReadyEventArgs e)
+    public static Task GetReady(DiscordClient sender, ReadyEventArgs e)
     {
         Bot.Logger.LogSuccess("Bot is ready to receive events.");
         return Task.CompletedTask;
