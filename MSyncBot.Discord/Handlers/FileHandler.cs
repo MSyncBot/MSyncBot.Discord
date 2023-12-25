82f233928075d184dc2d6810b5b70dd713928ced
Neelov Eugene <neelov.ev@mail.ru>
2023-12-25 09:17:57 +0300

    Added FileHandler.cs

---
+++ MSyncBot.Discord/Handlers/FileHandler.cs
@@ -0,0 +1,40 @@
+﻿using DSharpPlus.Entities;
+using MSyncBot.Types;
+using MSyncBot.Types.Enums;
+
+namespace MSyncBot.Discord.Handlers;
+
+public class FileHandler
+{
+    public async Task<MediaFile?> DownloadFileAsync(DiscordAttachment attachment)
+    {
+        try
+        {
+            using var httpClient = new HttpClient();
+            var fileData = await httpClient.GetByteArrayAsync(new Uri(attachment.Url));
+            var fileName = Guid.NewGuid().ToString();
+            var fileExtension = Path.GetExtension(attachment.FileName);
+
+            var mediaType = !string.IsNullOrEmpty(attachment.MediaType)
+                ? attachment.MediaType.Split('/')[0] 
+                : "document";
+
+            var fileType = mediaType switch
+            {
+                "image" => FileType.Photo,
+                "video" => FileType.Video,
+                "audio" => FileType.Audio,
+                _ => FileType.Document
+            };
+
+            var file = new MediaFile(fileName, fileExtension, fileData, fileType);
+            return file;
+        }
+        catch (Exception ex)
+        {
+            Bot.Logger.LogError(ex.ToString());
+        }
+
+        return null;
+    }
+}
