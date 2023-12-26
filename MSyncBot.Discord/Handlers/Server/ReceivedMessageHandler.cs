15bc40736fd517fa4aec74f838b4ea50892373f5
Neelov Eugene <neelov.ev@mail.ru>
2023-12-26 10:36:26 +0300

    Added new class for rework received messages

---
+++ MSyncBot.Discord/Handlers/Server/ReceivedMessageHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace MSyncBot.Discord.Handlers.Server;
+
+public class ReceivedMessageHandler
+{
+    private static ulong _lastUserId = 0;
+
+    public async Task ReceiveMessageAsync()
+    {
+        
+    }
+}
