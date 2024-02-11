97480e81a92936e53876e11b0a65ae83a4b5b6a9
Neelov Eugene <neelov.ev@mail.ru>
2024-02-11 23:35:16 +0300

    Deleted token

--- MSyncBot.Discord/Program.cs
+++ MSyncBot.Discord/Program.cs
@@ -6,7 +6,7 @@ internal class Program
 {
     private static async Task Main()
     {
-        var bot = new Bot("MTE4Mjc4NDE5MzA3MjIwOTk0MA.GJtdBD.nWe5YGDjoA3d8x957zzRkF1xYd1JYHKEToKt7k",
+        var bot = new Bot("",
             new MLogger(),
             new MDatabase.MDatabase("####", "####", "####", "####"));
         await bot.StartAsync();
