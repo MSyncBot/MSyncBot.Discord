356aa328a20d85d18f73b1c4af318d04f96e9ab1
Neelov Eugene <neelov.ev@mail.ru>
2024-02-13 09:35:41 +0300

    Added ModelConfig.cs for project configuration

---
+++ MSyncBot.Discord/ModelConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace MSyncBot.Discord.Config;
+
+public class ModelConfig
+{
+    public string? BotToken { get; set; }
+    
+    public string? ServerIp { get; set; }
+    public int ServerPort { get; set; }
+    
+    public string? DatabaseIp { get; set; }
+    public string? DatabaseName { get; set; }
+    public string? DatabaseUser { get; set; }
+    public string? DatabaseUserPassword { get; set; }
+}
